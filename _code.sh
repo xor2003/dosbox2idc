e090c8ad032443c1e465894625623ce00e71b63f
x0r <xorrezerv2@list.ru>
2019-07-18 18:07:38 +0300

    bug

--- _code.sh
+++ _code.sh
@@ -2,5 +2,5 @@
 S="0x31"
 echo "#include <idc.idc>" > _code.idc
 echo "static main(){" >> _code.idc
-grep -vF "F000:" LOGCPU.txt | perl -pe 's!^([0-9A-F]+):([0-9A-F]+).*!MakeCode((0x$1+$S)*0x10+0x$2);!g' >> _code.idc
+grep -vF "F000:" LOGCPU.txt | perl -pe 's!^([0-9A-F]+):([0-9A-F]+).*!MakeCode((0x$1+'$S')*0x10+0x$2);!g' >> _code.idc
 echo "}" >> _code.idc
